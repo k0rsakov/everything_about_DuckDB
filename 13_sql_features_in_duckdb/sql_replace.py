d9779a91f71ac46a304e29ff8d4417c81948660e
k0rsakov <korsakov.iyu@gmail.com>
2026-05-08 04:38:22 +0300

    Изменён проект everything_about_DuckDB: - 13 Добавлен пример использования replace

---
+++ 13_sql_features_in_duckdb/sql_replace.py
@@ -0,0 +1,19 @@
+import duckdb
+
+print(
+    "Все колонки:\n",
+    duckdb.query(
+        """
+        SELECT * FROM 'fake_data.parquet'
+        """
+    )
+)
+
+print(
+    "Замена значения колонки country на верхний регистр:\n",
+    duckdb.query(
+        """
+        SELECT * REPLACE (upper(country) AS country) FROM 'fake_data.parquet'
+        """
+    )
+)
