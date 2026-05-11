d6207e84765c396a25c5bdbf29740ab208ea0488
k0rsakov <korsakov.iyu@gmail.com>
2026-05-11 11:07:41 +0300

    Изменён проект everything_about_DuckDB: - 16 Небольшая оптимизация

--- 16_duckdb_minio_s3/03_read_parquet_from_s3_using_duckdb.py
+++ 16_duckdb_minio_s3/03_read_parquet_from_s3_using_duckdb.py
@@ -1,14 +1,24 @@
 import duckdb
 
+
+con = duckdb.connect()
+
+con.query(
+    """
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_use_ssl = false;
+    SET s3_url_style = 'path';
+    SET s3_access_key_id = 'minioadmin';
+    SET s3_secret_access_key = 'minioadmin';
+    """
+)
+
+
 print(
-    duckdb.query(
-        """
-        SET s3_endpoint = 'localhost:9000';
-        SET s3_use_ssl = false;
-        SET s3_url_style = 'path';
-        SET s3_access_key_id = 'minioadmin';
-        SET s3_secret_access_key = 'minioadmin';
-        
+    con.query(
+        """ 
         SELECT count()
         FROM 's3://prod/yellow_tripdata/2025/12/data.parquet';
         """
@@ -16,14 +26,8 @@ print(
 )
 
 print(
-    duckdb.query(
+    con.query(
         """
-        SET s3_endpoint = 'localhost:9000';
-        SET s3_use_ssl = false;
-        SET s3_url_style = 'path';
-        SET s3_access_key_id = 'minioadmin';
-        SET s3_secret_access_key = 'minioadmin';
-    
         SELECT *
         FROM 's3://prod/yellow_tripdata/2025/12/data.parquet'
         LIMIT 5;
