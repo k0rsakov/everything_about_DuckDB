a79e4fb80ed286430711f3ee275bb0fc14699631
k0rsakov <korsakov.iyu@gmail.com>
2026-05-11 10:03:46 +0300

    Изменён проект everything_about_DuckDB: - 16 Добавление сравнение .csv и .parquet

---
+++ 16_duckdb_minio_s3/06_comparison_csv_and_parquet_for_duckdb.py
@@ -0,0 +1,55 @@
+import duckdb
+
+con = duckdb.connect()
+
+con.query(
+    """
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_use_ssl = false;
+    SET s3_url_style = 'path';
+    SET s3_access_key_id = 'minioadmin';
+    SET s3_secret_access_key = 'minioadmin';
+    """
+)
+
+# .csv, .parquet
+con.query(
+    """
+    INSTALL fakeit FROM community;
+    LOAD fakeit;
+
+    CREATE TABLE fake_data AS
+    SELECT
+        fakeit_name_full() AS name,
+        fakeit_contact_email() AS email,
+        fakeit_address_city() AS city,
+        fakeit_address_country() AS country
+    FROM
+        generate_series(1, 1_000_000);
+    
+    COPY fake_data TO 's3://prod/fake_data/fake_data.csv' (FORMAT 'CSV');
+    COPY fake_data TO 's3://prod/fake_data/fake_data.parquet' (FORMAT 'PARQUET');
+    """
+)
+
+df_csv = con.query(
+    """
+    EXPLAIN ANALYZE
+    SELECT count() FROM 's3://prod/fake_data/fake_data.csv' 
+    """
+).df()
+
+df_parquet = con.query(
+    """
+    EXPLAIN ANALYZE
+    SELECT count() FROM 's3://prod/fake_data/fake_data.parquet' 
+    """
+).df()
+
+print("🧾 CSV:")
+print(df_csv.explain_value[0])
+
+print("\n🪵 PARQUET:")
+print(df_parquet.explain_value[0])
