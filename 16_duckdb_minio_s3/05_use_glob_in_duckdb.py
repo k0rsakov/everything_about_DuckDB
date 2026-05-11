e69b764e770e39f0bc09e207025bd4f40335780a
k0rsakov <korsakov.iyu@gmail.com>
2026-05-11 08:50:36 +0300

    Изменён проект everything_about_DuckDB: - 16 Добавлены примеры использования glob

---
+++ 16_duckdb_minio_s3/05_use_glob_in_duckdb.py
@@ -0,0 +1,103 @@
+import duckdb
+
+con = duckdb.connect()
+
+con.query(
+    """
+    INSTALL httpfs;
+    LOAD httpfs;
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_use_ssl = false;
+    SET s3_url_style = 'path';
+    SET s3_access_key_id = 'minioadmin';
+    SET s3_secret_access_key = 'minioadmin';
+    """
+)
+
+# 2020-2021-2022
+print(
+    con.query(
+        """
+        SELECT
+            date_trunc('year', tpep_pickup_datetime) AS pickup_year,
+            COUNT(*) AS trip_count
+        FROM
+            's3://prod/yellow_tripdata/202[0-2]/*/data.parquet'
+        WHERE
+            1=1
+        GROUP BY
+            1
+        -- Исключение выбросов
+        HAVING
+            trip_count > 1_000
+        ORDER BY
+            1
+        """
+    )
+)
+
+# 2023-2024-2025
+print(
+    con.query(
+        """
+        SELECT
+            date_trunc('year', tpep_pickup_datetime) AS pickup_year,
+            COUNT(*) AS trip_count
+        FROM
+            's3://prod/yellow_tripdata/202[3-5]/*/data.parquet'
+        WHERE
+            1=1
+        GROUP BY
+            1
+        -- Исключение выбросов
+        HAVING
+            trip_count > 1_000
+        ORDER BY
+            1
+        """
+    )
+)
+
+# 2020-2022-2024, чётные года
+print(
+    con.query(
+        """
+        SELECT
+            date_trunc('year', tpep_pickup_datetime) AS pickup_year,
+            COUNT(*) AS trip_count
+        FROM
+            's3://prod/yellow_tripdata/202[0,2,4]/*/data.parquet'
+        WHERE
+            1=1
+        GROUP BY
+            1
+        -- Исключение выбросов
+        HAVING
+            trip_count > 1_000
+        ORDER BY
+            1
+        """
+    )
+)
+
+print(
+    con.query(
+        """
+        SELECT
+            date_trunc('year', tpep_pickup_datetime) AS pickup_year,
+            EXTRACT(QUARTER FROM date_trunc('year', tpep_pickup_datetime)) AS pickup_quarter, 
+            COUNT(*) AS trip_count
+        FROM
+            's3://prod/yellow_tripdata/*/0[1-3]/data.parquet'
+        WHERE
+            1=1
+        GROUP BY
+            1, 2
+        -- Исключение выбросов
+        HAVING
+            trip_count > 1_000
+        ORDER BY
+            1, 2
+        """
+    )
+)
