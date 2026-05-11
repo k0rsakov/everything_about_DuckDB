7f4f264f50fa9911511d914974f77baf20070ef5
k0rsakov <korsakov.iyu@gmail.com>
2026-05-11 06:33:57 +0300

    Изменён проект everything_about_DuckDB: - 16 Добавлен пример сохранения parquet в с3 при использовании утки

---
+++ 16_duckdb_minio_s3/02_direct_copy_parquet_to_s3_using_duckdb.py
@@ -0,0 +1,21 @@
+import duckdb
+
+duckdb.query(
+    """
+    --INSTALL httpfs;
+    --LOAD httpfs;
+
+    SET s3_endpoint = 'localhost:9000';
+    SET s3_use_ssl = false;
+    SET s3_url_style = 'path';
+    SET s3_access_key_id = 'minioadmin';
+    SET s3_secret_access_key = 'minioadmin';
+    
+    COPY
+    (
+        SELECT *
+        FROM 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2025-12.parquet'
+    )
+    TO 's3://prod/yellow_tripdata/2025/12/data.parquet'
+    """
+)
