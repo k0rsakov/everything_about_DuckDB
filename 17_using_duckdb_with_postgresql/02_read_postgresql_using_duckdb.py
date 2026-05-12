6f703be552c0cc5f0471535c88d0ad6bada2ccc3
k0rsakov <korsakov.iyu@gmail.com>
2026-05-12 11:35:31 +0300

    Изменён проект everything_about_DuckDB: - 17 Добавлен пример чтения postgresql с использованием duckdb

---
+++ 17_using_duckdb_with_postgresql/02_read_postgresql_using_duckdb.py
@@ -0,0 +1,19 @@
+import duckdb
+
+con = duckdb.connect()
+
+con.query(
+    "ATTACH "
+    "'dbname=postgres "
+    "user=postgres "
+    "host=localhost "
+    "password=postgres' "
+    "AS db (TYPE postgres, SCHEMA 'public');"
+)
+
+print(con.query(
+    """
+    SELECT *
+    FROM db.tbl
+    """
+))
